27e2a06219b6d291bc60bbd98e3c7aff093c1dbe
KJ59 <klarasedi@gmail.com>
2025-02-17 22:49:09 +0100

    homework review changes

--- index.js
+++ index.js
@@ -51,20 +51,14 @@ const flight = {
     quality: 66.66659,
   };
   
-  const handLuggageMaxSize = {maxWidth: flight.baglimit.hand_width = 20, 
-                              maxHeight: flight.baglimit.hand_height = 40, 
-                              maxLength: flight.baglimit.hand_length = 55, 
-                              maxWight: flight.baglimit.hand_weight = 10}; 
-
-  const holdBagMaxSize = {maxWidth: flight.baglimit.hold_width = 81, 
-                          maxHeight: flight.baglimit.hold_height = 119, 
-                          maxLength: flight.baglimit.hold_length = 119, 
-                          maxWeight: flight.baglimit.hold_weight = 20,
-                          maxSumDimensions: flight.baglimit.hold_dimensions_sum = 319};
+  const luggageMaxSize = flight.baglimit;
 
   document.body.innerHTML += `<p>Destination country is <b>${flight.countryTo.name}</b> and departure country is <b> ${flight.countryFrom.name}.</b>  </p>`
   document.body.innerHTML += `<h4> Important: Carry-on luggage limitations</h4>`
-  document.body.innerHTML += `<p>Maximum weight of carry-on luggage is  <b>${holdBagMaxSize.maxWeight} kg</b>. Regarding metric limits, maximum height is  <b>${handLuggageMaxSize.maxHeight} cm</b>, 
-                              maximum length is  <b>${holdBagMaxSize.maxLength} cm</b> and maximum width is  <b>${holdBagMaxSize.maxWidth} cm</b>. There is also limitation on cumulative dimensions. It is <b> ${holdBagMaxSize.maxSumDimensions} cm</b>.</p>`
+  document.body.innerHTML += `<p>Maximum weight of carry-on luggage is <b> ${luggageMaxSize.hand_weight} kg</b>. Regarding metric limits, maximum height is  <b>${luggageMaxSize.hand_height} cm</b>, 
+                              maximum length is  <b>${luggageMaxSize.hand_length} cm</b> and maximum width is  <b>${luggageMaxSize.hand_width} cm</b>.</p>`
+  document.body.innerHTML +=  `<h4> Important: Checked luggage limitations </h4>
+                              <p>Regarding checked luggage, maximum height is <b> ${luggageMaxSize.hold_height} cm</b>, maximum width is <b>${luggageMaxSize.hold_width} cm</b>
+                              maximum weight is <b>${luggageMaxSize.hold_weight} kg</b> and maximum length is <b>${luggageMaxSize.hold_length} cm.</b> There is also limitation on cumulative dimensions. It is <b> ${luggageMaxSize.hold_dimensions_sum} cm</b>.</p>`
   document.body.innerHTML += `<h4> Extra costs of second luggage </h4>`                               
   document.body.innerHTML += `<p>In case you need to travel with two baggages, you will pay for the second one <b> ${Math.ceil(flight.conversion.EUR*(flight.bags_price['2']))} CZK.</b> </p>`
